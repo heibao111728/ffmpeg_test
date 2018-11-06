b66be6dd4828570c638586ae8bb7833126f34a81
yangdong <yangdong@boostiv.com>
2018-11-06 17:48:55 +0800

    optimization demuxer2, use callback function to get stream.

--- StreamManager/StreamManager.h
+++ StreamManager/StreamManager.h
@@ -4,20 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_BUFFER_SIZE (4 * 1024 * 1024)
-
 #define LOG(fmt, ...) fprintf(stdout, "[DEBUG] %s\n%s:%d:" fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
-class CStreamManager
+class stream_manager
 {
 private:
-    CStreamManager();
-    ~CStreamManager();
+    stream_manager();
+    ~stream_manager();
 
 public:
-    static CStreamManager* m_instance;
+    static stream_manager* m_instance;
 
-    static CStreamManager* get_instance()
+    static stream_manager* get_instance()
     {
         if (NULL != m_instance)
         {
@@ -25,26 +23,35 @@ public:
         }
         else
         {
-            m_instance = new CStreamManager();
+            m_instance = new stream_manager();
             return m_instance;
         }
     }
+
+    static int m_capacity;
+    static void set_capacity_size(int capacity_size)
+    {
+        m_capacity = capacity_size;
+    }
+
     /**
     *   description:
     *       read length of data to buffer.
     */
-    int read_data(void* op, unsigned char* buffer, int length);
+    int pull_data(void* op, unsigned char* buffer, int length);
 
     /**
     *   description:
     *       write data.
     */
-    int write_data(unsigned char* data, int length);
+    int push_data(unsigned char* data, int length);
+
+    int get_data_length();
+
 private:
     unsigned char* m_stream_data_buffer;
     int m_stream_start_point;
     int m_stream_end_point;
-    int m_capacity;
     int m_data_length;
 };
 
